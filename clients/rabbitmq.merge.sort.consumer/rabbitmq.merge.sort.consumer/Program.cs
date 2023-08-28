47eca67f1794a67eb09e63201c6f9e569f7d49d2
Seweryn Gładysz <seweryn.gladysz@kotrak.com>
2023-08-28 22:00:41 +0200

    initial commit

---
+++ clients/rabbitmq.merge.sort.consumer/rabbitmq.merge.sort.consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Hello, World!");
+
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "127.0.0.1",
+    Port = 5552,
+    Password = "algo",
+    UserName = "algo",
+};
+
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "hello_world_broker",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+    
+Console.WriteLine("Waiting for messages from broker: ");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received message: {message}");
+};
+
+channel.BasicConsume(
+    queue: "hello_world_broker",
+    autoAck: true,
+    consumer: consumer);
+    
+Console.Write("Press [enter] to exit");
+Console.ReadLine();
---
+++ clients/rabbitmq.merge.sort.producer/rabbitmq.merge.sort.producer/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "127.0.0.1",
+    Port = 5552,
+    Password = "algo",
+    UserName = "algo"
+};
+
+using var brokerConnection = connectionFactory.CreateConnection();
+using var channel = brokerConnection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "hello_world_broker",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+while (true)
+{
+    Console.Write("write message: ");
+    var userInput = Console.ReadLine();
+
+    if (userInput is null)
+    {
+        continue;
+    }
+
+    if (userInput.Equals(".exit"))
+    {
+        return 0;
+    }
+
+    var body = Encoding.UTF8.GetBytes(userInput);
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: "hello_world_broker",
+        basicProperties: null,
+        body: body);
+}
+
+
